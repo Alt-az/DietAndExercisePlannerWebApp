d0d45e154a23fc07d5d2e52bef0d77389bbc612e
Alt-az <235979@edu.p.lodz.pl>
2023-05-01 12:41:59 +0200

    home page update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './Components/Navbar'
-import Footer from './Components/Footer'
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Home/>
       <Footer/>
     </div>
   );
---
+++ src/Components/Home.js
@@ -0,0 +1,10 @@
+import HomeCarousel from "./HomeCarousel";
+import BenefitsDescription from "./BenefitsDescription";
+export default function Home(){
+    return(
+        <div>
+            <HomeCarousel/>
+            <BenefitsDescription/>
+        </div>
+    );
+}
